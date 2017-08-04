8de9b8264d789cf73149afeba7d3f5a425c7c24e
slanders <slanders@pindrop.com>
2017-08-04 12:07:43 -0400

    adding multi-decision ballot

--- test/TestBallot.sol
+++ test/TestBallot.sol
@@ -23,18 +23,21 @@ contract TestBallot {
     }
 
     function testCastVote(){
+        //create ballot with 2 decisions
         Ballot b = new Ballot();
         b.addDecision("President 2020");
         b.addOption(0, "trump");
         b.addOption(0, "perot");
+
+        //add owner as voter
         b.addVoter(b.getOwner());
 
-        var votes = new uint[](1);
-
-        votes[0] = uint(1);
-
+        //activate election
         b.activate();
 
+        // vote for perot (index = 1)
+        var votes = new uint[](1);
+        votes[0] = uint(1);
         b.castVote(votes);
 
         uint256 notVotedOptionCount = 0;
@@ -42,6 +45,37 @@ contract TestBallot {
 
         uint256 votedOptionCount = 1;
         Assert.equal(b.getOptionResults(0, 1), votedOptionCount, "Results should be 1");
+
+        b.close();
+    }
+
+    function testCastMultiDecisionVote(){
+        //create ballot with 2 decisions
+        Ballot b = new Ballot();
+        b.addDecision("President 2020");
+        b.addOption(0, "trump");
+        b.addOption(0, "perot");
+
+        b.addDecision("Governor 2020");
+        b.addOption(1, "Glavine");
+        b.addOption(1, "Smoltz");
+
+        //add owner as voter
+        b.addVoter(b.getOwner());
+
+        //activate election
+        b.activate();
+
+        // vote for perot & glavine (index = 1)
+        var votes = new uint[](2);
+        votes[0] = uint(1);
+        votes[1] = uint(0);
+        b.castVote(votes);
+
+        Assert.equal(b.getOptionResults(0, 0), 0, "Trump should be 0");
+        Assert.equal(b.getOptionResults(0, 1), 1, "Perot should be 1");
+        Assert.equal(b.getOptionResults(1, 0), 1, "Glavine should be 1");
+        Assert.equal(b.getOptionResults(1, 1), 0, "Smoltz should be 0");
 
         b.close();
     }
