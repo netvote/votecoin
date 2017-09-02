6fae2865e6e0cba0b7b5c4f5300dbe9e44b0537d
slanders <slanders@pindrop.com>
2017-09-02 16:52:15 -0400

    adding weighted private election

---
+++ contracts/elections/WeightedPrivateElection.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.11;
+
+import './WeightedElection.sol';
+import './PrivateElection.sol';
+
+contract WeightedPrivateElection is WeightedElection, PrivateElection {
+
+}
---
+++ test/TestWeightedPrivateElection.sol
@@ -0,0 +1,84 @@
+pragma solidity ^0.4.2;
+
+import "truffle/Assert.sol";
+import "../contracts/elections/WeightedPrivateElection.sol";
+
+contract TestWeightedPrivateElection {
+
+    function testCastVoteDefault(){
+
+        //create ballot with 2 decisions
+        WeightedPrivateElection b = new WeightedPrivateElection();
+        b.addDecision("President 2020");
+        b.addOption(0, "trump");
+        b.addOption(0, "perot");
+
+        //allow ballot to transact votecoin for voter
+        Votecoin v = new Votecoin();
+        b.setVotecoin(v);
+        v.mint(b.owner(), 100); //enough for a vote
+        v.approve(address(b), 100);
+
+        b.addVoter(b.owner());
+
+        Assert.equal(v.allowance(b.owner(), address(b)), 100, "Allowance should be 100");
+
+        //activate election
+        b.activate();
+
+        // vote for perot (index = 1)
+        var votes = new uint[](1);
+        votes[0] = uint(1);
+        b.castVote(votes);
+
+        Assert.equal(v.allowance(b.owner(), address(b)), 0, "Allowance should be 0");
+
+        uint256 notVotedOptionCount = 0;
+        Assert.equal(b.getOptionResults(0, 0), notVotedOptionCount, "Results should be 0");
+
+        uint256 votedOptionCount = 1;
+        Assert.equal(b.getOptionResults(0, 1), votedOptionCount, "Results should be 1");
+
+        b.close();
+    }
+
+    function testCastVoteWeighted(){
+
+        //create ballot with 2 decisions
+        WeightedPrivateElection b = new WeightedPrivateElection();
+        b.addDecision("President 2020");
+        b.addOption(0, "trump");
+        b.addOption(0, "perot");
+
+        //allow ballot to transact votecoin for voter
+        Votecoin v = new Votecoin();
+        b.setVotecoin(v);
+        v.mint(b.owner(), 100); //enough for a vote
+        v.approve(address(b), 100);
+
+        b.addVoter(b.owner());
+
+        Assert.equal(v.allowance(b.owner(), address(b)), 100, "Allowance should be 100");
+
+        b.setWeight(b.owner(), 2);
+
+        //activate election
+        b.activate();
+
+        // vote for perot (index = 1)
+        var votes = new uint[](1);
+        votes[0] = uint(1);
+        b.castVote(votes);
+
+        Assert.equal(v.allowance(b.owner(), address(b)), 0, "Allowance should be 0");
+
+        uint256 notVotedOptionCount = 0;
+        Assert.equal(b.getOptionResults(0, 0), notVotedOptionCount, "Results should be 0");
+
+        uint256 votedOptionCount = 2;
+        Assert.equal(b.getOptionResults(0, 1), votedOptionCount, "Results should be 1");
+
+        b.close();
+    }
+
+}
